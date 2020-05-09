e978bcc18cee2a2f77f8a1f10963f3402c357d82
thakgatso1 <thakgatso.chuene@eoh.com>
2020-05-10 01:58:57 +0200

    sample component

---
+++ demo/core/src/main/java/org/redquark/demo/core/constants/AppConstants.java
@@ -0,0 +1,12 @@
+package org.redquark.demo.core.constants;
+
+/**
+ * @author
+ * 
+ * This class has all the project related constants
+ */
+public final class AppConstants {
+	
+	public static final String URL = "https://jsonplaceholder.typicode.com/todos/";
+
+}
