477fd58ec81b1b348efe32ec25b774a79d641b42
thakgatso1 <thakgatso.chuene@eoh.com>
2020-05-24 03:04:02 +0200

    D14

---
+++ LTL_D14/core/src/main/java/org/redquark/demo/core/services/ReadJsonService.java
@@ -0,0 +1,13 @@
+package org.redquark.demo.core.services;
+
+/**
+ * @author 
+ * Service which will be exposed
+ */
+public interface ReadJsonService {
+
+	/**
+	 * @return JSON String
+	 */
+	public String getData();
+}
